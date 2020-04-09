9a30fe00beef5563d70d3c023beb9ca5e7d3e7b8
Chris Bellaro <chrisbellaro@Chriss-Air.home>
2020-04-09 10:32:23 -0400

    limited backend routes to only index and show

--- doodleduel_api/app/controllers/words_controller.rb
+++ doodleduel_api/app/controllers/words_controller.rb
@@ -1,5 +1,5 @@
 class WordsController < ApplicationController
-  before_action :set_word, only: [:show, :update, :destroy]
+  before_action :set_word, only: [:show]
 
   # GET /words
   def index
@@ -13,39 +13,10 @@ class WordsController < ApplicationController
     render json: @word
   end
 
-  # POST /words
-  def create
-    @word = Word.new(word_params)
-
-    if @word.save
-      render json: @word, status: :created, location: @word
-    else
-      render json: @word.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /words/1
-  def update
-    if @word.update(word_params)
-      render json: @word
-    else
-      render json: @word.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /words/1
-  def destroy
-    @word.destroy
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_word
       @word = Word.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
-    def word_params
-      params.require(:word).permit(:word_to_draw)
-    end
 end
